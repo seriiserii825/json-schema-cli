b3b6d45b51577f3f3fc4d1a66c19efad40cadc98
serii burduja <seriiburduja@gmail.com>
2025-10-17 15:21:53 +0300

    feat: need to fionish

--- src/modules/fetchApiFromFileUrl.ts
+++ src/modules/fetchApiFromFileUrl.ts
@@ -1,7 +1,9 @@
+import getConfig from "./getConfig.js";
 import {getWpUrl} from "./getWpUrl.js";
 
-export default async function fetchApiFromFilesUrls(urls: string[]): void {
+export default async function fetchApiFromFileUrl(urls: string[]): Promise<void> {
   const wp_url = getWpUrl();
+  const config = getConfig();
   console.log("wp_url", wp_url);
   // Placeholder function to fetch API from file URLs
   // Implement the logic to read files from URLs and process them as needed
